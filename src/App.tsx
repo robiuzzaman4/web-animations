98dc6c7831ea6519a130033bb13d8ffa8cd5c00e
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-01-20 22:33:27 +0600

    [DONE] done up to css transition with javascript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 
 function App() {
+  const name = "Ruhan Zaman";
+  const namArr = name.split("");
   return (
     <div className="main">
-      <div className="box-1">
-
+      <div className="container">
+        {namArr?.map((item, i) => (
+          <span className="item" style={{ transitionDuration: `${i * 40}ms` }}>
+            {item}
+          </span>
+        ))}
       </div>
     </div>
   );
